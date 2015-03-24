d670cd3aaec775f86de01c0990fd69feedf52c46
Kalle Immonen <immonen.kp@gmail.com>
2015-03-24 20:33:11 +0200

    Added flash sequence handling.

--- ledFlasher.ino
+++ ledFlasher.ino
@@ -1,7 +1,14 @@
+#include <EEPROM.h>
+//const byte EEPROM_START = 0;
+const byte MAX_LED_ON = 10; //10ms hopefully
+const byte HOLD_CAMERA_PIN_UP = 30;
+const byte FLASH_SEQUNCE_SIZE = 8; //4 delays, 4 ons,
+
+byte {FLASH_SEQUNCE_SIZE} flashSequence;
 void setup()
 {
     readDefaultsFromEeprom();
-    initOutputs()
+    initOutputs();
 }
 
 void initOutputs()
@@ -37,14 +44,13 @@ boolean shouldTriggerLedFlash()
 void triggerCamera()
 {
     digitalWrite(CAMERA_PIN, HIGH);
-    waitFor(30)
+    waitFor(HOLD_CAMERA_PIN_UP);
     digitalWrite(CAMERA_PIN, LOW);
 }
 
 void triggerFlashing()
 {
-    byte size = flashSequence.length;
-    for (byte i = 0; i < size; i++ )
+    for (byte i = 0; i < FLASH_SEQUNCE_SIZE; i++ )
     {
         int time = flashSequence[i];
         if (i % 2 == 0)
@@ -60,6 +66,10 @@ void triggerFlashing()
 
 void turnLedOn(int time)
 {
+    if (time > MAX_LED_ON)
+    {
+        time = 1;
+    }
     digitalWrite(LED_PIN, HIGH);
     waitFor(time);
     digitalWrite(LED_PIN, LOW);
@@ -67,28 +77,56 @@ void turnLedOn(int time)
 
 void readDefaultsFromEeprom()
 {
-    read_from_eeprom();
+    byte i;
+    for (byte i = 0; i < FLASH_SEQUNCE_SIZE; i++)
+    {
+        flashSequence {i} = EEPROM.read(i);
+    }
 }
 
-void writeDefaultsToEeprom()
+void updateFlashSequence(byte {} newFlashSequnce)
 {
-    //TODO Flash following:
-    //Flashing sequnce
-    //Sensor port & trigger level
+    for (byte i = 0; i < FLASH_SEQUNCE_SIZE; i++)
+    {
+        if (newFlashSequnce {i} != flashSequence {i})
+        {
+            EEPROM.write(i, flashSequence {i});
+        }
+    }
+    flashSequence = newFlashSequnce;
+}
+void updateSensorConfig()
+{
+    //TODO Sensor port & trigger level
 }
 
 void readSerial()
 {
+
     //Pulses definion: set of onOff
     /**
-    1. byte: delay after signal < 1000ms
-    2. byte: light on time < 10ms
-    3. byte: delay delay after next < 1000ms
-    4. byte: light on time < 10ms
+    1. byte: MODE SELECTION: Flash configure(0xFA),Sensor configure(0xFB)
+
+    **Flash configures
+    2. byte: delay after signal < 1000ms
+    3. byte: light on time < 10ms
+    4. byte: delay delay after next < 1000ms
+    5. byte: light on time < 10ms
     IF 0x00 sent - means end of message
+
+    **Sensor configure
+    2.byte:
+        -1bit Analog/digital mode
+        -1bit Threshold/limit mode
+        -1bit Below/above limit trigger
+        -1bit Reserved
+        -4bit PORT number - use all bits!
+
+    3-4. bytes 1024 Trigger level (only needs 11bits)
+
+
     **/
 
     //SAVE SETTINGS TO EEPROM
-
 }
 
